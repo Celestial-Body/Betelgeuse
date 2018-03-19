41feda5c0b78e1d99f7e396ba9d7040a01eff3d9
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-19 17:08:57 -0400

    Create UnexpectedValueException.php

---
+++ src/Exception/UnexpectedValueException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+namespace Betelgeuse\Validator\Exception;
+
+/**
+ * UnexpectedValueException.
+ */
+class UnexpectedValueException extends \UnexpectedValueException implements ExceptionInterface
+{   
+}
