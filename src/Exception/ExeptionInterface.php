a93af24546d0939a18646ab3f64a9e4ff7e39c2f
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-19 12:10:03 -0400

    Create ExeptionInterface.php

---
+++ src/Exception/ExeptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+namespace Betelgeuse\Validator\Exception;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface
+{   
+}
