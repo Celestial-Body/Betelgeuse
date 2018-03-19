d8fec55eedc761feff9e93974a123b47e40f9dac
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-19 11:00:47 -0400

    Create the config class for email validation.

--- src/Email/Config.php
+++ src/Email/Config.php
@@ -1,1 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
 
+namespace Betelgeuse\Validator\Email;
+
+use Betelgeuse\Validator\ConfigInterface;
+
+/**
+ * Config.
+ */
+class Config implements ConfigInterface
+{
+}
