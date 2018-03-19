f57e61ae7f0bc819072cfcb994d5ad33fcc78932
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-19 11:14:17 -0400

    Add version declaration.

--- version.php
+++ version.php
@@ -1,1 +1,11 @@
+<?php
+declare(strict_types=1);
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
 
+define('BETELGEUSE_VERSION_ID', 010000);
+define('BETELGEUSE_VERSION', '1.0.0');
