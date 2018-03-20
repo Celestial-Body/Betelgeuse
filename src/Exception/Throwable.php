c5706d804b07d95ac959bea83d6d530bd19db202
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-20 14:03:15 -0400

    Create Throwable.php

---
+++ src/Exception/Throwable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+namespace Betelgeuse\Validator\Exception;
+
+/**
+ * Throwable
+ */
+interface Throwable
+{
+    public function getMessage();
+    public function getCode();
+    public function getFile();
+    public function getLine();
+    public function getTrace();
+    public function getTraceAsString();
+    public function getPrevious();
+    public function __toString();
+}
