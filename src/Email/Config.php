de8f2832924ad0754e2f91fc48d27ca08a23be3f
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-20 14:34:30 -0400

    Update Config.php

--- src/Email/Config.php
+++ src/Email/Config.php
@@ -8,10 +8,15 @@
  
 namespace Betelgeuse\Validator\Email;
 
+use Betelgeuse\Validator\Exception\InvalidArgumentException;
+use Betelgeuse\Validator\Exception\UnexpectedValueException;
+use Betelgeuse\Validator\Exception\DomainException;
+use Betelgeuse\Validator\ConfigInterface;
+
 /**
  * Config.
  */
-class Config implements \Betelgeuse\Validator\ConfigInterface
+class Config implements ConfigInterface
 {
     
     /**
@@ -36,12 +41,12 @@ class Config implements \Betelgeuse\Validator\ConfigInterface
     public function __construct(array $options = [])
     {
         if (empty($options)) {
-            throw new \Betelgeuse\Validator\Exception\InvalidArgumentException(\sprintf(
+            throw new InvalidArgumentException(\sprintf(
                 'The config data type is invalid or empty. Data type: %s.',
                 (string) \gettype($options);
             ));
         } elseif (\depth($options) != 2) {
-            throw new \Betelgeuse\Validator\Exception\DomainException(\sprintf(
+            throw new DomainException(\sprintf(
                 'The config depth is incorrect. Array depth: %s.',
                 (string) \depth($options)
             ));
@@ -55,7 +60,7 @@ class Config implements \Betelgeuse\Validator\ConfigInterface
                     'mode' =>    '',
                     'plugins' => ''
                 ])) {
-                    throw new \Betelgeuse\Validator\Exception\UnexpectedValueException(
+                    throw new UnexpectedValueException(
                         'The option does not exist or is no longer used.'
                     );
                 }
@@ -63,21 +68,21 @@ class Config implements \Betelgeuse\Validator\ConfigInterface
                     if ($option === 'mode') {
                         foreach ($val as $key) {
                             if (!\is_string($key)) {
-                                throw new \Betelgeuse\Validator\Exception\InvalidArgumentException(\sprintf(
+                                throw new InvalidArgumentException(\sprintf(
                                     'The data key has an invalid data type. Data type: %s',
                                     \gettype($key)
                                 ));
                             }
                             if ($key != 'internal'
                                 && $key != 'egulias') {
-                                throw new \Betelgeuse\Validator\Exception\DomainException('The key value is unknown.');
+                                throw new DomainException('The key value is unknown.');
                             }
                         }
                     }
                     if ($option === 'plugins') {
                         foreach ($val as $key) {
                             if (!\is_string($key)) {
-                                throw new \Betelgeuse\Validator\Exception\InvalidArgumentException(\sprintf(
+                                throw new InvalidArgumentException(\sprintf(
                                     'The data key has an invalid data type. Data type: %s',
                                     \gettype($key)
                                 ));
@@ -85,12 +90,12 @@ class Config implements \Betelgeuse\Validator\ConfigInterface
                             if ($key != 'rcfvalidation'
                                 && $key != 'dnscheckvalidation'
                                 && $key != 'spoofcheckvalidation') {
-                                throw new \Betelgeuse\Validator\Exception\DomainException('The key value is unknown.');
+                                throw new DomainException('The key value is unknown.');
                             }
                         }
                     }
                 } else {
-                    throw new \Betelgeuse\Validator\Exception\InvalidArgumentException(\sprintf(
+                    throw new InvalidArgumentException(\sprintf(
                         'The option data type is invalid. Data type: %s',
                         \gettype($val)
                     ));
