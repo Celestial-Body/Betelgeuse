499833e2094de6439673190a40fcc33321354fd1
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-21 16:51:03 -0400

    Add constructor method.

--- src/Email/Validator.php
+++ src/Email/Validator.php
@@ -9,10 +9,33 @@
 namespace Betelgeuse\Validator\Email;
 
 use Betelgeuse\Validator\ValidatorInterface;
+use Betelgeuse\Validator\ConfigInterface;
 
 /**
  * Validator.
  */
 class Validator extends AbstractValidator implements ValidatorInterface
 {
+    
+    /**
+     * @var array $config_options The config options.
+     */
+    private $config_options = [];
+    
+    /**
+     * Set the config options to use.
+     *
+     * @param mixed $configOptions The validator config.
+     *
+     * @throws InvalidArgumentException If it is not a validator config.
+     *
+     * @return void
+     */
+    public function __construct(ValidatorInterface $configOptions)
+    {
+        if (!($configOptions instanceof Config)) {
+            throw new InvalidArgumentException('The config is not a validator config');
+        }
+        $this->config_options = $configOptions->getOptions();
+    }
 }
