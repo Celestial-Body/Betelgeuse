c11f1345783db25b197710abf3afeb3a2906a60c
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-21 19:44:20 -0400

    Update Validator.php

--- src/Email/Validator.php
+++ src/Email/Validator.php
@@ -8,6 +8,10 @@
  
 namespace Betelgeuse\Validator\Email;
 
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\DNSCheckValidation;
+use Egulias\EmailValidator\Validation\SpoofCheckValidation;
+use Egulias\EmailValidator\Validation\MultipleValidationWithAnd;
 use Betelgeuse\Validator\Exception\InvalidArgumentException;
 use Betelgeuse\Validator\ValidatorInterface;
 use Betelgeuse\Validator\AbstractValidator;
@@ -27,7 +31,7 @@ class Validator extends AbstractValidator implements ValidatorInterface
     /**
      * Set the config options to use.
      *
-     * @param ValidatorInterface $configOptions The validator config.
+     * @param mixed $configOptions The validator config.
      *
      * @throws InvalidArgumentException If it is not a validator config.
      *
@@ -39,5 +43,74 @@ class Validator extends AbstractValidator implements ValidatorInterface
             throw new InvalidArgumentException('The config is not a validator config');
         }
         $this->config_options = $configOptions->getOptions();
+        if (isset($this->config_options['plugin'])) {
+            $this->config_options['plugins'] += $this->config_options['plugin'];
+        }
+    }
+    
+    /**
+     * Validate the test case.
+     *
+     * @param mixed $testCase The test case to validate.
+     *
+     * @return array Return an array with a state key containing TRUE or FALSE
+     *               True equals the test case is valid and FALSE if the test case
+     *               is invalid. Also return error messages if the config options array
+     *               requests it.
+     */
+    public function validate($testCase)
+    {
+        $retArray = [
+            'valid' => \true;
+        ];
+        if (empty($testCase) || $testCase == '') {
+            $retArray['valid'] = \false;
+            return (array) $retArray;
+        } elseif (!\is_string($testCase)) {
+            $retArray['valid'] = \false;
+            return (array) $retArray;
+        } else {
+            if ($this->config_options['mode'] == 'egulias') {
+                $validator = new EmailValidator();
+                $validationList = [];
+                if (\in_array('rfcvalidation',
+                    $this->config_options['plugins']
+                ) {
+                    $validationList += [
+                        new RFCValidation()
+                    ];
+                }
+                if (\in_array('spoofcheckvalidation',
+                    $this->config_options['plugins']
+                ) {
+                    $validationList += [
+                        new SpoofCheckValidation()
+                    ];
+                }
+                if (\in_array('dnscheckvalidation',
+                    $this->config_options['plugins']
+                ) {
+                    $validationList += [
+                        DNSCheckValidation()
+                    ];
+                }
+                $multipleValidations = new MultipleValidationWithAnd(
+                    $validationList
+                );
+                if (!empty($multipleValidations)) {
+                    if ($validator->isValid($testCase, $multipleValidations)) {
+                        $retArray['valid'] => \true;
+                        return (array) $retArray;
+                    }
+                } else {
+                    if ($validator->isValid($testCase)) {
+                        $retArray['valid'] => \true;
+                        return (array) $retArray;
+                    }
+                }
+            }
+        }
+        $retArray['valid'] => \false;
+        return (array) $retArray;
     }
 }
