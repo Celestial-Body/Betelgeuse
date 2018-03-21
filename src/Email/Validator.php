550853f80285ad49aa150f6c8343af74313ad003
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-21 15:56:10 -0400

    Create Validator.php

---
+++ src/Email/Validator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+namespace Betelgeuse\Validator\Email;
+
+use Betelgeuse\Validator\ValidatorInterface;
+
+/**
+ * Validator.
+ */
+class Validator extends AbstractValidator implements ValidatorInterface
+{
+}
