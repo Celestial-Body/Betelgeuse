60c4fe8829074f17e357d37cab90009244ea33d6
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-23 18:16:29 -0400

    Update ConfigInterface.php

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -14,29 +14,6 @@ namespace Betelgeuse\Validator;
 interface ConfigInterface
 {
     
-    /**
-     * Create and validate a new config.
-     *
-     * @param array $options The options to pass.
-     *
-     * @throws InvalidArgumentException If the data type for each option is invalid.
-     * @throws InvalidArgumentException If the options array is an empty array.
-     * @throws UnexpectedValueException If the option does not exist.
-     * @throws InvalidArgumentException If the data is invalid.
-     * @throws DomainException          If the options array depth is incorrect.
-     * @throws DomainException          If the key value is unknown.
-     *
-     * @return void Return nothing.
-     */
     public function __construct(array $options = []);
-    
-    /**
-     * Get the config options.
-     *
-     * @throws InvalidArgumentException If the config options array
-     *                                  is empty.
-     *
-     * @return array The config options array.
-     */
     public function getOptions();
 }
