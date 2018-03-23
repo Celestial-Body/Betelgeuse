dd064553ca36f6e0695f71e20627adebb10eeeb8
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-23 19:39:00 -0400

    Update Messages.php

--- src/Regex/Messages.php
+++ src/Regex/Messages.php
@@ -1,1 +1,20 @@
+<?php
+/**
+ * Celestial Body.
+ *
+ * @link    <https://github.com/Celestial-Body/Betelgeuse> Github repository.
+ * @license <https://github.com/Celestial-Body/Betelgeuse/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+namespace Betelgeuse\Validator\Regex;
 
+/**
+ * Messages.
+ */
+class Messages
+{
+    
+    const NO_TESTCASE_PROVIDED = 'No testcase was provided.';
+    const WRONG_DATA_TYPE      = 'The test case has an incorrect data type.';
+    const NO_PATTERN_DETECTED  = 'No pattern was detected.';
+}
